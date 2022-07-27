f49893bcaab77ab11d85dbbf371334f858caaf4f
Skeleturtle <val742val742@gmail.com>
2022-07-27 13:58:10 -0600

    setup OpenGL / Window creation and rendering.

--- valor.cpp
+++ valor.cpp
@@ -1,16 +1,72 @@
 // Valor engine by Valores M.
 // Using GLFW to create window and process input
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "window.h"
+#include <iostream>
 
-valorWindow vw;
+// Header
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-int main() {
+// Settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
 
-	// Make window
-	vw.makeWindow();
-	// Window cleanup command
-	vw.cleanup();
-	return(0);
+int main(void)
+{
+    // Init and config for glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // Window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Valor Engine", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    };
+    glfwMakeContextCurrent(window);
+
+    // Load glad pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    };
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // rendering commands here            
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    return 0;
+
 };
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+};
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+};
--- window.cpp
+++
@@ -1,9 +0,0 @@
-#include "Window.h"
-
-GLFWwindow* window() {
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-	
-};
-void valorWindow::cleanup() {
-	glfwDestroyWindow(window);
-};
